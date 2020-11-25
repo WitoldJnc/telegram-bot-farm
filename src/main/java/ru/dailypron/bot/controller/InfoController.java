4b403e7dd3bbb3fd9973ea5af6bd3efa6d0c7f0c
witold <witold.jnc@gmail.com>
2020-11-25 15:45:25 +0200

    add info endpoint for cronjob

--- src/main/java/ru/dailypron/bot/controller/InfoController.java
+++ src/main/java/ru/dailypron/bot/controller/InfoController.java
@@ -1,9 +1,16 @@
 package ru.dailypron.bot.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/info")
 public class InfoController {
+
+    @GetMapping("/")
+    public ResponseEntity<String> getInfo(){
+        return ResponseEntity.ok("checked");
+    }
 }
