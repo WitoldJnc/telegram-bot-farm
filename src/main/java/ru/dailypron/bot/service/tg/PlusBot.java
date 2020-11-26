3b6a1d0ebe540e6e52efb79c4499a237eed5523a
witold <witold.jnc@gmail.com>
2020-11-26 15:36:37 +0200

    add another one bot with more functionals

---
+++ src/main/java/ru/dailypron/bot/service/tg/PlusBot.java
@@ -0,0 +1,112 @@
+package ru.dailypron.bot.service.tg;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import ru.dailypron.bot.model.DailyEntity;
+import ru.dailypron.bot.repo.CustomDailyEntityService;
+import ru.dailypron.bot.repo.ExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class PlusBot extends TelegramLongPollingBot {
+    @Autowired
+    private Environment env;
+
+    @Autowired
+    private ExceptionHandler exceptionHandler;
+
+    @Autowired
+    private CustomDailyEntityService customDailyEntityService;
+
+    @Override
+    public String getBotUsername() {
+        return String.valueOf(env.getProperty("tg.bot.plus.api.name"));
+    }
+
+    @Override
+    public String getBotToken() {
+        return String.valueOf(env.getProperty("tg.bot.plus.api.key"));
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        Optional<DailyEntity> dailyEntity = Optional.of(customDailyEntityService.findRandom())
+                .map(x -> customDailyEntityService.findRandom())
+                .orElse(customDailyEntityService.findAny());
+
+        String AD = "by @pantyOfDestiny \n\n";
+
+        if (update.hasMessage()) {
+            if (update.getMessage().hasText()) {
+                if (update.getMessage().getText().equals("/start")) {
+                    try {
+                        execute(sendInlineKeyBoardMessage(update.getMessage().getChatId(), AD + "find your own guilty pleasure", "start", false, null));
+                    } catch (TelegramApiException e) {
+                        e.printStackTrace();
+                        exceptionHandler.postToInfo("on init message \n" + e);
+                    }
+                } else {
+                    try {
+                        execute(sendInlineKeyBoardMessage(update.getMessage().getChatId(), AD, "start", false, null));
+                    } catch (TelegramApiException e) {
+                        exceptionHandler.postToInfo("on empty message and send ad \n" + e);
+                    }
+                }
+            }
+        } else if (update.hasCallbackQuery()) {
+            if (!update.getCallbackQuery().getData().equals("show-callback")) {
+
+            } try {
+                execute(sendInlineKeyBoardMessage(update.getCallbackQuery().getMessage().getChatId(), dailyEntity.get().getTitle(), "more", true, dailyEntity.get().getUrl()));
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+                exceptionHandler.postToInfo("on get callback by button \n" + e);
+            }
+        }
+    }
+
+
+    public static SendMessage sendInlineKeyBoardMessage(long chatId, String message, String capture, Boolean showLink, String url) {
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+        InlineKeyboardButton inlineKeyboardButton1 = new InlineKeyboardButton();
+
+        inlineKeyboardButton1.setText(capture);
+        inlineKeyboardButton1.setCallbackData("Button \"more\" has been pressed");
+
+
+        List<InlineKeyboardButton> keyboardButtonsRow1 = new ArrayList<>();
+        List<InlineKeyboardButton> keyboardButtonsRow2 = new ArrayList<>();
+
+        keyboardButtonsRow1.add(inlineKeyboardButton1);
+
+        if (showLink) {
+            InlineKeyboardButton button = new InlineKeyboardButton();
+            button.setText("show");
+            button.setUrl(url);
+            button.setCallbackData("show-callback");
+            keyboardButtonsRow1.add(button);
+        }
+
+        List<List<InlineKeyboardButton>> rowList = new ArrayList<>();
+        rowList.add(keyboardButtonsRow1);
+        rowList.add(keyboardButtonsRow2);
+        inlineKeyboardMarkup.setKeyboard(rowList);
+
+        SendMessage sm = new SendMessage();
+        sm.setChatId(String.valueOf(chatId));
+        sm.setText(String.format("<b>%s</b>", message));
+        sm.setParseMode("HTML");
+        sm.setReplyMarkup(inlineKeyboardMarkup);
+        return sm;
+    }
+}
