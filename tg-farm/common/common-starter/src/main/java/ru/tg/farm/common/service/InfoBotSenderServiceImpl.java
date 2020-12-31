0b61e4afe1637472d90cc02b3e821048e06490ef
witold <witold.jnc@gmail.com>
2020-12-31 15:26:58 +0200

    add consume at log topic and impl sending error messages to private tg bot

--- tg-farm/common/common-starter/src/main/java/ru/tg/farm/common/service/InfoBotSenderServiceImpl.java
+++ tg-farm/common/common-starter/src/main/java/ru/tg/farm/common/service/InfoBotSenderServiceImpl.java
@@ -1,20 +1,49 @@
 package ru.tg.farm.common.service;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Component;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Value;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.tg.farm.common.model.KafkaEntity;
+import ru.tg.farm.common.model.LogType;
 import ru.tg.farm.common.repository.InfoBotSenderSerivce;
 
-@Component
-@Slf4j
-public class InfoBotSenderServiceImpl implements InfoBotSenderSerivce {
+public class InfoBotSenderServiceImpl extends TelegramLongPollingBot implements InfoBotSenderSerivce {
+    @Value("${tg.bot.info.api.name}")
+    private String botName;
+
+    @Value("${tg.bot.info.api.key}")
+    private String botKey;
+
+    @Value("${tg.bot.info.api.chat}")
+    private String chatId;
+
 
     @Override
-    @KafkaListener(topics = "${kafka.log.topic}", groupId = "${spring.kafka.consumer.group-id}",
-            containerFactory = "KafkaEntityKafkaListenerContainerFactory")
-    public void listenTopic(KafkaEntity message) {
-        //todo logic post to bot
+    public void snedToInfoBot(KafkaEntity logEntity) throws TelegramApiException {
+        if (logEntity.getType().equals(LogType.ERROR)) {
+            String errorMessage = String.format("<b>%s</b>: %s \n\n %s", logEntity.getSubSystem(), logEntity.getMessage(), logEntity.getCreated());
+            SendMessage sendMessage = new SendMessage(chatId, errorMessage);
+            sendMessage.setParseMode("HTML");
+            execute(sendMessage);
+        }
     }
 
+    @SneakyThrows
+    @Override
+    public void onUpdateReceived(Update update) {
+        execute(new SendMessage(chatId, "init"));
+    }
+
+    @Override
+    public String getBotUsername() {
+        return this.botName;
+    }
+
+    @Override
+    public String getBotToken() {
+        return this.botKey;
+    }
 }
